b5ae1cc6f1e14a619ee469792b7b937e8561a191
friermor <david@classwave.com>
2012-01-09 10:51:12 -0800

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CwMockups::Application.config.secret_token = 'ce57d1923ae1e98d2e8a2edd58f508793fbed0648dafad18e9884fe9e1b500259883e606f9ee741c476852015069dd859a7e83941ab94708b4a63e3ca67a5fd4'
